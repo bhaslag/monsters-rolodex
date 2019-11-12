96016e5e9856baf455c2032ae0508f9f55eaf0fc
William Haslag <wihas@smile.fr>
2019-11-12 11:14:33 +0100

    Updates

---
+++ precache-manifest.7c0592fced501a92f6293318ef844492.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04b8f5a0e743e95bf54ce82fa66f4556",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "22389874654cee03d2fc",
+    "url": "/monsters-rolodex/static/css/main.ac30142f.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monsters-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "22389874654cee03d2fc",
+    "url": "/monsters-rolodex/static/js/main.c55a657d.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
